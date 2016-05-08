dbd9ea4ee9034cb2cbf111305d86a7a8e6258168
Celso Fernandes <fernandes@zertico.com>
2016-05-08 11:40:35 -0300

    Better handling on API denying credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   respond_to :html
   before_action :check_softlayer_login
   before_action :set_current_user
+  rescue_from Softlayer::Errors::SoapError, with: :handle_softlayer_exception
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -23,5 +24,15 @@ class ApplicationController < ActionController::Base
     if cookies.signed[:api_user].nil? and cookies.signed[:api_key].nil?
       redirect_to login_path
     end
+  end
+
+  def handle_softlayer_exception(exception)
+    return exception_invalid_login if exception.message.match /Invalid API token./
+  end
+
+  def exception_invalid_login
+    cookies.signed[:api_user] = nil
+    cookies.signed[:api_key] = nil
+    redirect_to login_path, alert: "API returned 'Invalid Credentials', Please Login Again"
   end
 end
